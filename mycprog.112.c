07827570235b93147d41bd7dcd6d04f839cf0ac2
chinnajee <chinnajee.rao@gmail.com>
2017-11-28 04:37:17 +0000

    from AWS vm

---
+++ mycprog.112.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+           char name[20];
+          printf("Hello friend, What is your name?\n");
+          scanf("%s",&name);
+          printf("Hello %s Welcome to c programming",name);
+}
